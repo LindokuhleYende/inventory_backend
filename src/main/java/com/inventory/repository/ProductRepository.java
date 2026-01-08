d8d4e34296b5ebc71db08edb9724b48304a82c97
Lindokuhle <yendelindokuhle2@gmail.com>
2026-01-08 10:51:46 +0200

    Added a securityConfig b3.n/ ean

--- src/main/java/com/inventory/repository/ProductRepository.java
+++ src/main/java/com/inventory/repository/ProductRepository.java
@@ -3,8 +3,11 @@ package com.inventory.repository;
 import com.inventory.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.*;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findBySku(String sku);
 
